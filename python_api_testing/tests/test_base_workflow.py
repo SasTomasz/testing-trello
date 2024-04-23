4905b321700521c7bd321af149aef6f24c797502
Tomasz Sas <tomaszsas.dev@gmail.com>
2024-04-23 14:07:36 +0200

    introduce to test assertpy

--- python_api_testing/tests/test_base_workflow.py
+++ python_api_testing/tests/test_base_workflow.py
@@ -1,6 +1,7 @@
 from http import HTTPStatus
 
 from dotenv import dotenv_values
+from assertpy import assert_that
 
 env = dotenv_values("../../.env")
 api_key = env.get("TRELLO_API_KEY")
@@ -10,7 +11,7 @@ api_token = env.get("TRELLO_API_TOKEN")
 def test_create_new_board_should_return_status_200(create_a_new_board):
     board = create_a_new_board
     board_response = board.get_a_board_body()
-    assert board_response.status_code == HTTPStatus.OK
+    assert assert_that(board_response.status_code).is_equal_to(HTTPStatus.OK)
 
 
 def test_new_board_should_have_a_correct_name():
@@ -27,5 +28,5 @@ def test_adding_new_card_should_return_status_200(create_a_new_board):
     first_list_id = board.get_info_about_lists_on_board().json()[0]["id"]
 
     card_response = board.create_a_new_card(card_name, first_list_id)
-    assert card_response.status_code == 200
+    assert assert_that(card_response.status_code).is_equal_to(200)
 
